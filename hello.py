638080c85cd1e06ad594f6ab1a4e1430cf3cca9f
Dana Fujikawa <danaf@essentialinstructions.ca>
2024-07-29 12:35:17 -0700

    set to v4

--- hello.py
+++ hello.py
@@ -9,12 +9,15 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-print("This is a test v3")
+print("This is a test v4")
 
 @app.route('/')
 def hello():
 	return 'Hello, World v3'
 
+#app.run(debug=True, host='::', port=5000)
+
 if __name__ == "__main__":
-	#app.run(debug=True, host='0.0.0.0', port=5000)
-	app.run(debug=True, host='::', port=5000)
+	app.run(debug=True, host='0.0.0.0', port=5000)
+	
+
