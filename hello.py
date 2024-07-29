de9327c747d855010b156f12fde69f65a02bce48
Dana Fujikawa <danaf@essentialinstructions.ca>
2024-07-29 10:30:38 -0700

    switch to ipv6

--- hello.py
+++ hello.py
@@ -16,4 +16,5 @@ def hello():
 	return 'Hello, World v3'
 
 if __name__ == "__main__":
-	app.run(debug=True, host='0.0.0.0', port=5000)
+	#app.run(debug=True, host='0.0.0.0', port=5000)
+	app.run(debug=True, host='::', port=5000)
